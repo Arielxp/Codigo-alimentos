6e8d408d8fa4573f4b7245ca4e6c693e11044048
Arielxp <156966119+Arielxp@users.noreply.github.com>
2024-05-29 17:28:09 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -3,4 +3,5 @@ void ingresarProductos(char productos[][3][30], float costos[], int cantidades[]
 void imprimirInventario(char productos[][3][30], float costos[], int cantidades[], int total);
 int buscarProductoPorNombre(char productos[][3][30], char nombreBuscado[], int total);
 void imprimirProductoPorIndice(char nombreBuscado[], char productos[][3][30], float costos[], int cantidades[], int indice);
+void editarProducto(char nombreBuscado[], char productos[][3][30], float costos[], int cantidades[], int total);
 void eliminarProducto(char productos[][3][30], float costos[], int cantidades[], int total, char nombreBuscado[]);
